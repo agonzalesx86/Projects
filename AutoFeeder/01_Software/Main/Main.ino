97758e5fe059d1a1edeebc34f278435f01385bbe
Alex-Mac <agonzalesx86@gmail.com>
2018-12-12 00:25:35 -0800

    commit

---
+++ AutoFeeder/01_Software/Main/Main.ino
@@ -0,0 +1,27 @@
+
+/*
+Info that will need to get worked out in the software:
+- scheduling
+- app control
+- switching between manual control and automated timing
+
+Need to being establishing the logic for switching between the two modes of operating.
+1. manual mode (user requests a feed through an app or something). may need to use an analog input to keep it simple and get it going.
+2. automated scheduling mode based on a schedule originally set by a user.
+
+First version:
+- user input from an analog HIGH from a physical tactile button which will be the trigger. Some implementation to prevent toggle will be needed as well.
+note: will need to define states as well.
+
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
