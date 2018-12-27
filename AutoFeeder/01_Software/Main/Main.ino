5161e14142d1f9e75888c35447edd040dd654e2f
Alexander Gonzales <agonzalesx86@gmail.com>
2018-12-27 15:30:47 -0800

    commit

--- AutoFeeder/01_Software/Main/Main.ino
+++ AutoFeeder/01_Software/Main/Main.ino
@@ -13,15 +13,58 @@ First version:
 - user input from an analog HIGH from a physical tactile button which will be the trigger. Some implementation to prevent toggle will be needed as well.
 note: will need to define states as well.
 
-
 */
+
+#include "main.h"
+
+
+/* constants */
+const int buttonPin=1;
+const int motorpwmPin = 2;
+
+/* calibrations and variables */
+int buttonState;
+float motorpwmRR = 1; // deg/sec
+float motorpwmFR = 1; // deg/sec
+float motordutyCmd = 1;
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(motorpwmPin,OUTPUT);
+  pinMode(buttonPin,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  requestMonitor();
+  if(buttonState==HIGH){
+    feed();
+  }
+}
 
+
+
+/*========================================================*/
+/********************FUNCTIONS*****************************/
+/*========================================================*/
+
+
+void feed_basic(){
+  // might need to implement a ramp up function when turning on the motor or a rate limiter.
+  analogWrite(motorpwmPin,HIGH);
+}
+
+void feed(){
+  motordutyCmd = final_lmting(motorpwmRR,);
+  t1 = millis();
+}
+
+float final_lmting(rr,x1,t1){
+  t2 = millis()
+  x_fnl = rr*(t2-t1) + x1;  
+  return x_fnl
+}
+
+void requestMonitor(){
+  digitalRead(buttonPin);
 }
